6d837740ba88d9c037a6310f35a76959b3107599
Maksim Terpilowski <maximtrp@gmail.com>
2021-12-11 13:21:46 +0300

    last.fm recommendations fetching fixed

--- mufi/mufi_recs.py
+++ mufi/mufi_recs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from random import shuffle
 import os
 import re
 import sys
+from random import shuffle
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # Login data paths
 paths = list(map(os.path.expanduser, ['~/.lastfm', '~/.config/mufi/.lastfm']))
 
 # Terminal colors
-
-
-class term:
+class Term:
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
     DARKCYAN = '\033[36m'
@@ -27,7 +25,7 @@ class term:
 
 
 def login(drv, user, password):
-    login_field = drv.find_element_by_id('id_username')
+    login_field = drv.find_element_by_id('id_username_or_email')
     if login_field:
         login_field.send_keys(user)
 
@@ -37,8 +35,8 @@ def login(drv, user, password):
     return drv
 
 
-def get_login_data(paths):
-    for path in paths:
+def get_login_data(config_paths):
+    for path in config_paths:
         if os.path.exists(path) and os.path.isfile(path):
             with open(path) as file:
                 user, password = file.read().split()
@@ -47,28 +45,28 @@ def get_login_data(paths):
 
 
 def nav_artists(drv):
-    drv.get('https://www.last.fm/home/artists')
+    drv.get('https://www.last.fm/music/+recommended/artists')
     return drv
 
 
 def nav_albums(drv):
-    drv.get('https://www.last.fm/home/albums')
+    drv.get('https://www.last.fm/music/+recommended/albums')
     return drv
 
 
 def get_recom_artists(drv):
-    artists_elems = drv.find_elements_by_class_name('recs-feed-item--artist')
+    artists_elems = drv.find_elements_by_class_name('music-recommended-artists-artist')
     if artists_elems:
         results = []
         for artist_elem in artists_elems:
             artist_name = artist_elem.find_element_by_xpath(
-                './/h3[@class="recs-feed-title"]').text
+                './/h3[@class="music-recommended-artists-artist-name"]').text
             listeners = artist_elem.find_element_by_xpath(
-                './/p[@class="recs-feed-description"]').text.replace(',', ' ').strip()
+                './/p[@class="music-recommended-artists-artist-stats"]').text#.replace(',', ' ').strip()
             listeners = re.sub(r'\D', '', listeners)
             listeners = int(listeners) if listeners else ''
             context = artist_elem.find_element_by_xpath(
-                './/div[@class="context"]').text.strip()
+                './/p[@class="music-recommended-artists-context"]').text.strip()
             artist = {'name': artist_name,
                       'listeners': listeners, 'context': context}
 
@@ -77,30 +75,77 @@ def get_recom_artists(drv):
     return None
 
 
+# def get_recom_artists(drv):
+#     artists_elems = drv.find_elements_by_class_name('recs-feed-item--artist')
+#     if artists_elems:
+#         results = []
+#         for artist_elem in artists_elems:
+#             artist_name = artist_elem.find_element_by_xpath(
+#                 './/h3[@class="recs-feed-title"]').text
+#             listeners = artist_elem.find_element_by_xpath(
+#                 './/p[@class="recs-feed-description"]').text.replace(',', ' ').strip()
+#             listeners = re.sub(r'\D', '', listeners)
+#             listeners = int(listeners) if listeners else ''
+#             context = artist_elem.find_element_by_xpath(
+#                 './/div[@class="context"]').text.strip()
+#             artist = {'name': artist_name,
+#                       'listeners': listeners, 'context': context}
+
+#             results.append(artist)
+#         return results
+#     return None
+
+
 def get_recom_albums(drv):
-    albums_elems = drv.find_elements_by_class_name('recs-feed-inner-wrap')
+    albums_elems = drv.find_elements_by_class_name('music-recommended-albums-item')
     if albums_elems:
         results = []
         for album_elem in albums_elems:
             album_name = album_elem.find_element_by_xpath(
-                './/h3[@class="recs-feed-title"]').text
+                './/h3[@class="music-recommended-albums-item-name"]').text
             artist_name = album_elem.find_element_by_xpath(
-                './/p[@class="recs-feed-description"]/a').text
+                './/p[@class="music-recommended-albums-album-artist"]/a').text
             listeners_text = album_elem.find_element_by_xpath(
-                './/p[@class="recs-feed-description"]').text
+                './/div[@class="music-recommended-albums-album-info"]').text
             listeners_matches = re.search(
                 r'\d+ listeners', re.sub(r'[^\w\s]', '', listeners_text))
             listeners = int(
                 re.sub(r'\D', '', listeners_matches[0])) if listeners_matches else ''
 
             context = album_elem.find_element_by_xpath(
-                './/div[@class="context"]').text.strip()
+                './/p[@class="music-recommended-albums-album-context"]').text.strip()
             artist = {'artist': artist_name, 'album': album_name,
                       'context': context, 'listeners': listeners}
 
             results.append(artist)
         return results
     return None
+
+
+# def get_recom_albums(drv):
+#     albums_elems = drv.find_elements_by_class_name('recs-feed-inner-wrap')
+#     if albums_elems:
+#         results = []
+#         for album_elem in albums_elems:
+#             album_name = album_elem.find_element_by_xpath(
+#                 './/h3[@class="recs-feed-title"]').text
+#             artist_name = album_elem.find_element_by_xpath(
+#                 './/p[@class="recs-feed-description"]/a').text
+#             listeners_text = album_elem.find_element_by_xpath(
+#                 './/p[@class="recs-feed-description"]').text
+#             listeners_matches = re.search(
+#                 r'\d+ listeners', re.sub(r'[^\w\s]', '', listeners_text))
+#             listeners = int(
+#                 re.sub(r'\D', '', listeners_matches[0])) if listeners_matches else ''
+
+#             context = album_elem.find_element_by_xpath(
+#                 './/div[@class="context"]').text.strip()
+#             artist = {'artist': artist_name, 'album': album_name,
+#                       'context': context, 'listeners': listeners}
+
+#             results.append(artist)
+#         return results
+#     return None
 
 
 def sort(lst, key, rev, num):
@@ -124,10 +169,10 @@ def print_artists(lst, context=True, comfy=False, verbose=0):
         if verbose == 1:
             print(artist['name'], listeners, sep='')
         elif verbose == 2:
-            print(term.BOLD, artist['name'], term.END, listeners, sep='')
+            print(Term.BOLD, artist['name'], Term.END, listeners, sep='')
         elif verbose >= 3:
-            print('[%d] ' % (i+1), term.BOLD, artist['name'],
-                  term.END, listeners, sep='')
+            print('[%d] ' % (i+1), Term.BOLD, artist['name'],
+                  Term.END, listeners, sep='')
         else:
             print(artist['name'])
 
@@ -145,10 +190,10 @@ def print_albums(lst, context=True, comfy=False, verbose=0):
                      item['listeners']) if item['listeners'] else ''
 
         if verbose >= 2:
-            print('[%d] ' % (i+1), term.BOLD, item['artist'],
-                  term.END, ' - ', item['album'], listeners, sep='')
+            print('[%d] ' % (i+1), Term.BOLD, item['artist'],
+                  Term.END, ' - ', item['album'], listeners, sep='')
         elif verbose == 1:
-            print(term.BOLD, item['artist'], term.END,
+            print(Term.BOLD, item['artist'], Term.END,
                   ' - ', item['album'], listeners, sep='')
         else:
             print(item['artist'], '-', item['album'],)
@@ -162,7 +207,7 @@ def print_albums(lst, context=True, comfy=False, verbose=0):
 def init_drv(headless=True, wait=15):
 
     options = webdriver.ChromeOptions()
-    options.headless = headless
+    options.headless = False #headless
     login_url = 'https://secure.last.fm/login'
 
     drv = webdriver.Chrome(options=options)
@@ -171,7 +216,6 @@ def init_drv(headless=True, wait=15):
     return drv
 
 
-# if __name__ == '__main__':
 def main():
 
     from argparse import ArgumentParser
@@ -205,21 +249,43 @@ def main():
 
     login_data = get_login_data(paths)
     if not login_data:
-        print('No login data found. Please say `echo login password > ~/.lastfm` in terminal.\
-            You can also use `~/.config/mufi/.lastfm` file to store your login data')
+        print(
+            'No login data found. Please say `echo login password > ~/.lastfm` in terminal.\n'
+            'You can also use `~/.config/mufi/.lastfm` file to store your login data')
         sys.exit(1)
 
     user, password = login_data
     login(drv, user, password)
 
     if artists:
+        artists_list = []
         nav_artists(drv)
-        artists = sort(get_recom_artists(
-            drv), key=orderby, rev=True, num=number)
-        print_artists(artists, context=context, verbose=verbose)
+        while True:
+            artists_list += sort(get_recom_artists(
+                drv), key=orderby, rev=True, num=number)
+
+            next_page_btn = drv.find_elements_by_class_name('pagination-next')
+            if not next_page_btn:
+                break
+            next_link = next_page_btn[0].find_element_by_tag_name('a')
+            drv.get(next_link.get_property('href'))
+
+        print_artists(artists_list, context=context, verbose=verbose)
     else:
+        albums_list = []
         nav_albums(drv)
-        albums = sort(get_recom_albums(drv), key=orderby, rev=True, num=number)
-        print_albums(albums, context=context, verbose=verbose)
+        while True:
+            albums_list += sort(get_recom_albums(drv), key=orderby, rev=True, num=number)
+            next_page_btn = drv.find_elements_by_class_name('pagination-next')
+
+            if not next_page_btn:
+                break
+            next_link = next_page_btn[0].find_element_by_tag_name('a')
+            drv.get(next_link.get_property('href'))
+
+        print_albums(albums_list, context=context, verbose=verbose)
 
     drv.quit()
+
+if __name__ == '__main__':
+    main()
